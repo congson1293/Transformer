5792a43f46f04f06be96417d361f17344b7c6d08
sontc <congson1293@gmail.com>
2021-04-09 16:45:45 +0700

    ...

--- Beam.py
+++ Beam.py
@@ -6,7 +6,7 @@ import math
 def init_vars(src, model, src_vocab, trg_vocab, opt):
     
     init_tok = trg_vocab.bos_idx
-    src_mask = (src != src_vocab.pad_token_id).unsqueeze(-2).to(opt.device)
+    src_mask = (src != src_vocab.pad_token_id).to(opt.device)
     e_output = model.encoder(src, src_mask)
     
     outputs = torch.LongTensor([[init_tok]]).to(opt.device)
@@ -50,7 +50,7 @@ def beam_search(src, model, src_vocab, trg_vocab, opt):
     model.eval()
     outputs, e_outputs, log_scores = init_vars(src, model, src_vocab, trg_vocab, opt)
     eos_token = trg_vocab.eos_idx
-    src_mask = (src != src_vocab.pad_token_id).unsqueeze(-2)
+    src_mask = (src != src_vocab.pad_token_id).to(opt.device)
     ind = None
     for i in range(2, opt.max_trg_len):
     
